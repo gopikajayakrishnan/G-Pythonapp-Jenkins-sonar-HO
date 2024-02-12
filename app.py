276d17e2af360425086be8bea7ca19f41b235e5d
gopikajayakrishnan <126472900+gopikajayakrishnan@users.noreply.github.com>
2024-02-12 11:38:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ images = [
     "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
 ]
 
-@app.route('/')
-def index():
+
     url = random.choice(images)
     return render_template('index.html', url=url)
 
